cb453fbc72d2ab3a0c2eb5e4fce66c5e0c487b86
EllaLi86 <ell024@ucsd.edu>
2025-11-12 01:40:25 -0800

    update js file

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
-console.log('IT’S ALIVE!');
+console.log('IT\'S ALIVE!');
 
 function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+    return Array.from(context.querySelectorAll(selector));
 }
