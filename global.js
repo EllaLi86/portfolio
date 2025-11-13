867119dbd69024b02ebb5946e47fd43424425eea
EllaLi86 <ell024@ucsd.edu>
2025-11-12 21:49:19 -0800

    Update global.js

--- global.js
+++ global.js
@@ -3,19 +3,19 @@ console.log('IT\'S ALIVE!');
 function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
-// Step 2.1: Get an array of all nav links into a variable
-const navLinks = $$("nav a");
+// // Step 2.1: Get an array of all nav links into a variable
+// const navLinks = $$("nav a");
 
-// Step 2.2: Find the link to the current page
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-);
+// // Step 2.2: Find the link to the current page
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+// );
 
-// Add the 'current' class to the found link
-if (currentLink) {
-  // or if (currentLink !== undefined)
-  currentLink.classList.add('current');
-}
+// // Add the 'current' class to the found link
+// if (currentLink) {
+//   // or if (currentLink !== undefined)
+//   currentLink.classList.add('current');
+// }
 
 let pages = [
   { url: '', title: 'Home' },
@@ -24,6 +24,11 @@ let pages = [
   { url: 'resume/', title: 'Resume' },
   { url: 'https://github.com/ftlaiLB9', title: 'GitHub' }
 ];
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"  // Local server
+    : "/portfolio/"; // Change this to your actual GitHub repo name
+
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
